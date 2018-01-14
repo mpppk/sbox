368a338ff850a57dd7d2814f255671d8aad8747a
mpppk <niboshiporipori@gmail.com>
2018-01-14 16:53:55 +0900

    Implement browse command

--- cmd/browse.go
+++ cmd/browse.go
@@ -2,20 +2,38 @@ package cmd
 
 import (
 	"fmt"
+	"net/url"
+	"path"
+	"strings"
 
+	"github.com/skratchdot/open-golang/open"
 	"github.com/spf13/cobra"
 )
 
+var contents string
 var browseCmd = &cobra.Command{
 	Use:   "browse",
 	Short: "browse page",
-	Long: ``,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+		names := strings.Split(args[0], "/")
+		pageName := names[len(names)-1]
+		if len(names) > 1 {
+			projectName = names[len(names)-2]
+		}
+		if len(names) > 2 {
+			serverName = names[len(names)-3]
+		}
 
-		fmt.Println("browse command is not impolemented yet")
+		values := url.Values{}
+		values.Add("body", contents)
+		pageURL := path.Join(serverName, projectName, pageName)
+		pageURLWithQuery := fmt.Sprintf("%s?%s", pageURL, values.Encode())
+		open.Run(pageURLWithQuery)
 	},
 }
 
 func init() {
+	browseCmd.PersistentFlags().StringVar(&contents, "contents", "", "page contents")
 	RootCmd.AddCommand(browseCmd)
 }
--- cmd/root.go
+++ cmd/root.go
@@ -9,15 +9,13 @@ import (
 )
 
 var cfgFile string
+var projectName string
+var serverName string
 
-// RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "sbox",
 	Short: "CLI for Scrapbox",
-	Long: ``,
-// Uncomment the following line if your bare application
-// has an action associated with it:
-//	Run: func(cmd *cobra.Command, args []string) { },
+	Long:  ``,
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -32,6 +30,8 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sbox.yaml)")
+	RootCmd.PersistentFlags().StringVar(&projectName, "project", "", "target project")
+	RootCmd.PersistentFlags().StringVar(&serverName, "server", "http://scrapbox.io", "target server")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -40,7 +40,7 @@ func initConfig() {
 		viper.SetConfigFile(cfgFile)
 	}
 
-	viper.SetConfigName(".sbox") // name of config file (without extension)
+	viper.SetConfigName(".sbox")           // name of config file (without extension)
 	viper.AddConfigPath(os.Getenv("HOME")) // adding home directory as first search path
 	viper.AutomaticEnv()                   // read in environment variables that match
 
