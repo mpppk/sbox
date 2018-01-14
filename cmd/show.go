3e395d1049e63737f1863447e09e4565b67474f2
mpppk <niboshiporipori@gmail.com>
2018-01-14 17:55:10 +0900

    Implement show command

--- cmd/show.go
+++ cmd/show.go
@@ -1,18 +1,28 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/mpppk/go-scrapbox/scrapbox"
+	"github.com/mpppk/sbox/utl"
 	"github.com/spf13/cobra"
 )
 
 // showCmd represents the show command
 var showCmd = &cobra.Command{
 	Use:   "show",
-	Short: "show page",
-	Long: ``,
+	Short: "show page contents",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("show command is not impolemented yet")
+		targetPage := utl.ParsePagePath(args[0], projectName, serverName)
+		client := scrapbox.NewClient(nil)
+		contents, _, err := client.Pages.GetText(context.Background(), targetPage.Project, targetPage.Name)
+		if err != nil {
+			fmt.Println("failed to fetch page from " + targetPage.String())
+		}
+
+		fmt.Println(contents)
 	},
 }
 
